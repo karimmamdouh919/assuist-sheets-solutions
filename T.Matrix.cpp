0970140174f344b51647fbad34750623931e93f8
كريم ممدوح عبد القادر عبد العزيز <121877133+karimmamdouh919@users.noreply.github.com>
2023-10-17 20:43:43 +0300

    Added a 2d array algorithm

---
+++ T.Matrix.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+int n;
+cin >> n;
+int arr[n][n];
+
+for(int i=0 ; i<n ; i++)
+    for(int j=0 ; j<n ; j++)
+        cin >> arr[i][j];
+
+
+        int x=n-1,prime=0,second=0;
+for(int i=0 ; i<n ; i++){
+    for(int j=0 ; j<n ; j++)
+    {
+        if(i==j)prime+=arr[i][j];
+        if(j==x)second+=arr[i][j];
+    }
+    x--;
+}
+if(prime-second<0)cout << -1*(prime-second);
+else cout << prime-second;
+    return 0;
+}
+
